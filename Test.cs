364cf14800392e0d1f9b3cb65f4e4875c215be37
harroo <harroo@protonmail.com>
2022-01-03 21:15:33 +1100

    add hashing sha256

--- Test.cs
+++ Test.cs
@@ -7,10 +7,12 @@ public static class TestProgram {
 
     public static void Main (string[] args) {
 
-        Console.WriteLine("Testing::  Logging"); TestLogging();
-        Console.WriteLine("Testing::  SplitString"); SplitString();
-        Console.WriteLine("Testing::  Timers"); Timers();
-        Console.WriteLine("Testing::  Serialization"); SerializationTest();
+        Console.WriteLine("\nTesting::  Logging"); TestLogging();
+        Console.WriteLine("\nTesting::  SplitString"); SplitString();
+        Console.WriteLine("\nTesting::  Timers"); Timers();
+        Console.WriteLine("\nTesting::  Serialization"); SerializationTest();
+        Console.WriteLine("\nTesting::  Encryption"); EncryptionTest();
+        Console.WriteLine("\nTesting::  Hashing"); HashingTest();
 
         Console.WriteLine("Tests concluded.");
     }
@@ -60,6 +62,30 @@ public static class TestProgram {
         TestClass y = (TestClass)Serialization.Deserialize(data);
 
         Console.WriteLine(y.x.ToString() + ": " + y.text);
+    }
+
+    public static void EncryptionTest () {
+
+        string value = "hi im a secret message";
+        Console.WriteLine(value);
+
+        byte[] data0 = System.Text.Encoding.ASCII.GetBytes(value);
+        byte[] data1 = Encryption.Encrypt(data0, "testkey012345678");
+        // byte[] data2 = Encryption.Decrypt(data0, "wrongtestkey1234");
+        byte[] data3 = Encryption.Decrypt(data1, "testkey012345678");
+
+        Console.WriteLine(data0.Length.ToString() + ": " + System.Text.Encoding.ASCII.GetString(data0));
+        Console.WriteLine(data1.Length.ToString() + ": " + System.Text.Encoding.ASCII.GetString(data1));
+        // Console.WriteLine(data2.Length.ToString() + ": " + System.Text.Encoding.ASCII.GetString(data2));
+        Console.WriteLine(data3.Length.ToString() + ": " + System.Text.Encoding.ASCII.GetString(data3));
+    }
+
+    public static void HashingTest () {
+
+        string value = "hi im a secret message";
+
+        Console.WriteLine(value);
+        Console.WriteLine(Hashing.Sha256Hash(value));
     }
 
     [Serializable]
